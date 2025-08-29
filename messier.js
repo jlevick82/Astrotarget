5f5658a7ed57a7fcf4bfb9683347478921ec570f
jlevick82 <jaylevick@yahoo.com>
2025-08-29 00:24:29 -0400

    Create messier.js

---
+++ messier.js
@@ -0,0 +1,29 @@
+const messier = [
+  {
+    name: "M1 – Crab Nebula",
+    ra: 5.575,
+    dec: 22.0,
+    mag: 8.4,
+    size: "6x4",
+    type: "Supernova Remnant",
+    img: "https://cdn.spacetelescope.org/archives/images/screen/heic0515a.jpg"
+  },
+  {
+    name: "M31 – Andromeda Galaxy",
+    ra: 0.71,
+    dec: 41.3,
+    mag: 3.4,
+    size: "190x60",
+    type: "Galaxy",
+    img: "https://cdn.spacetelescope.org/archives/images/screen/heic1502a.jpg"
+  },
+  {
+    name: "M42 – Orion Nebula",
+    ra: 5.58,
+    dec: -5.45,
+    mag: 4.0,
+    size: "65x60",
+    type: "Emission Nebula",
+    img: "https://cdn.spacetelescope.org/archives/images/screen/heic0601a.jpg"
+  }
+];
