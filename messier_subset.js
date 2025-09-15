e49a01b6366269de6c8fb2ba7c95a1634d506de7
jlevick82 <jaylevick@yahoo.com>
2025-09-15 18:59:42 -0400

    update

---
+++ messier_subset.js
@@ -0,0 +1,98 @@
+window.MESSIER_SUBSET = [
+  {
+    "id": "M31",
+    "name": "Andromeda Galaxy",
+    "ra": 0.712,
+    "dec": 41.269,
+    "mag": 3.4,
+    "type": "Galaxy"
+  },
+  {
+    "id": "M45",
+    "name": "Pleiades",
+    "ra": 3.792,
+    "dec": 24.117,
+    "mag": 1.6,
+    "type": "Open Cluster"
+  },
+  {
+    "id": "M42",
+    "name": "Orion Nebula",
+    "ra": 5.588,
+    "dec": -5.391,
+    "mag": 4.0,
+    "type": "Nebula"
+  },
+  {
+    "id": "M13",
+    "name": "Hercules Globular",
+    "ra": 16.694,
+    "dec": 36.461,
+    "mag": 5.8,
+    "type": "Globular Cluster"
+  },
+  {
+    "id": "M57",
+    "name": "Ring Nebula",
+    "ra": 18.892,
+    "dec": 33.03,
+    "mag": 8.8,
+    "type": "Planetary Nebula"
+  },
+  {
+    "id": "M27",
+    "name": "Dumbbell Nebula",
+    "ra": 19.997,
+    "dec": 22.721,
+    "mag": 7.5,
+    "type": "Planetary Nebula"
+  },
+  {
+    "id": "M1",
+    "name": "Crab Nebula",
+    "ra": 5.575,
+    "dec": 22.017,
+    "mag": 8.4,
+    "type": "Supernova Remnant"
+  },
+  {
+    "id": "M51",
+    "name": "Whirlpool Galaxy",
+    "ra": 13.494,
+    "dec": 47.195,
+    "mag": 8.4,
+    "type": "Galaxy"
+  },
+  {
+    "id": "M101",
+    "name": "Pinwheel Galaxy",
+    "ra": 14.054,
+    "dec": 54.349,
+    "mag": 7.9,
+    "type": "Galaxy"
+  },
+  {
+    "id": "M33",
+    "name": "Triangulum Galaxy",
+    "ra": 1.554,
+    "dec": 30.66,
+    "mag": 5.7,
+    "type": "Galaxy"
+  },
+  {
+    "id": "M8",
+    "name": "Lagoon Nebula",
+    "ra": 18.061,
+    "dec": -24.386,
+    "mag": 6.0,
+    "type": "Nebula"
+  },
+  {
+    "id": "M20",
+    "name": "Trifid Nebula",
+    "ra": 18.059,
+    "dec": -23.02,
+    "mag": 6.3,
+    "type": "Nebula"
+  }
+];
